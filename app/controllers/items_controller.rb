1e815e33ed5d73ae1b932b89dbee9896d039e6e8
mitstaku <takuya.mitsuhori@gmail.com>
2023-12-09 13:35:50 +0900

    商品情報編集画面

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,13 +15,30 @@ class ItemsController < ApplicationController
     end
   end
 
-    def index
-      @items = Item.all.order(created_at: :desc)
-    end
+  def index
+    @items = Item.all.order(created_at: :desc)
+  end
 
-    def show
-      @item = Item.find(params[:id])
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user_id
+      redirect_to root_path
     end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
