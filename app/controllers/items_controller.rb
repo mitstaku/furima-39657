5fefb631fb8afb13dcceb0ddb16e4f712ba8bf12
mitstaku <takuya.mitsuhori@gmail.com>
2023-12-17 19:02:25 +0900

    edit修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,9 +23,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    return if current_user.id == @item.user_id
-
-    redirect_to root_path
+    if @item.user_id == current_user.id && @item.order.nil?
+    else
+      redirect_to root_path
+    end
   end
 
   def update
