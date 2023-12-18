8915fa792cc62de725df222690b70243349466a0
mitstaku <takuya.mitsuhori@gmail.com>
2023-12-18 23:43:29 +0900

    マイグレーション修正

--- db/migrate/20231213153418_create_orders.rb
+++ db/migrate/20231213153418_create_orders.rb
@@ -1,9 +1,11 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
-    create_table :orders do |t|
-      t.references :user,              null: false, foreign_key: true
-      t.references :item,              null: false, foreign_key: true
-      t.timestamps
+    unless table_exists?(:orders)  
+      create_table :orders do |t|
+        t.references :user,              null: false, foreign_key: true
+        t.references :item,              null: false, foreign_key: true
+        t.timestamps
+      end
     end
   end
 end
