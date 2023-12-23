e1c2637cb1030c8a8759beba4f9b11b76b1fa650
mitstaku <takuya.mitsuhori@gmail.com>
2023-12-23 18:02:22 +0900

    key修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
 
 # どの公開鍵を利用してデプロイするか
-set :ssh_options, auth_methods: ['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDEhx6/de89T/f0TGEb59GI9cqgx3IHIJsp8OtzD1Lb9cY6doTpRAm2j0/p1+l++W+3Q44to5aloaMInu8oiEbi0mfjDtmWwhlCRzyr1PEV+wRxEhV+1enoxr+A7+JZCaqaMdgdAFJJ8AOtAy7aV9XVFZgY/acOQUBbm0t+xhCAfRu8gZQlY8lPGaWRAwBI2LYnlmbDW2pLqUKe129XLPVpmuX+pgfwLgyKQhNQ+rNFOZ0K/0sC4apchiPA3pDFmCtR+3W8lVeSDgYPwFia5tV+2Qskn8EjZHNdPapije/y4/AsJcZwE4CWfPfk2cq6CE+EpoXVHEINRAp/7WIPaspfHo8lZkJ9kn7O/qGpDu9W1HzNzn8zwFY89RnYgXW8WldPfQUBB41z/H71Vi9Mid5Lwq13jcA/5LZ/rHlMXgcy9piz1ZzVBqiWJdxFIbKuTHkytZGS8UsjTk1VNiAAzleN9dJqJhunW0xG5KrmxgCqg4nbKCkDwiKcpagHYsYkNWsUNIJ39dzEKSNflecRZDHcTZ1ydT5IOLBRvZbpjK4iNqau+52U4SdfktQK09JjviCcngYNJf+XCXsJ/A+01BfqTrQXL+7Xod1VREUUfRioyc5/utDohZmT2Q6biPej0kZxksHEQ1yuTn6DWYHKi20PujKnIl5bEMZPdy2erA0aqw== ec2-user@ip-172-31-43-83.ap-northeast-1.compute.internal'],
+set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/furima.pem'] 
 
 # プロセス番号を記載したファイルの場所
